3f60ad511461b5f85d42a28fbeb6a49bc2143302
miyatheta <miyatheta69@gmail.com>
2018-03-10 22:02:07 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   #get 'static_pages/home'
   root 'static_pages#home'
@@ -7,13 +6,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'help_us'
   get '/about', to: 'static_pages#about', as: 'jikoshoukai'
   get '/contact' => 'static_pages#contact'
-=======
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
->>>>>>> parent of 2913da2... Finish routes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

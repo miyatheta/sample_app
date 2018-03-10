667d18b62dcbec0a524c7a17e14feef20f638746
miyatheta <miyatheta69@gmail.com>
2018-03-10 21:53:15 +0900

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   devise_for :users
   #get 'static_pages/home'
   root 'static_pages#home'
@@ -6,6 +7,13 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'help_us'
   get '/about', to: 'static_pages#about', as: 'jikoshoukai'
   get '/contact' => 'static_pages#contact'
+=======
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+>>>>>>> parent of 2913da2... Finish routes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

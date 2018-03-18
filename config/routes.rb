b2c06d0ff3d77b8abc68e57cd825e912b6e02d97
miyatheta <miyatheta69@gmail.com>
2018-03-18 17:02:44 +0900

    users show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SampleApp::Application.routes.draw do
+  get 'users/show'
+
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
   resources :users, only: [:show]
-
-  #get 'static_pages/home'
   root 'static_pages#home'
-  #match '/help', to: 'static_pages#help', via: 'get'
+
   get '/help', to: 'static_pages#help', as: 'help_us'
   get '/about', to: 'static_pages#about', as: 'jikoshoukai'
   get '/contact' => 'static_pages#contact'

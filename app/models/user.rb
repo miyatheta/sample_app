a49056f2454b615a6a3f34cc56a30402294f7bc7
miyatheta <miyatheta69@gmail.com>
2018-03-31 19:04:55 +0900

    follow followed view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationships", dependent: :destroy
-  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable

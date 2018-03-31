9de8ad1ae44cf5a29f89921ce3c8784be26c4bbe
miyatheta <miyatheta69@gmail.com>
2018-03-31 18:00:27 +0900

    Make a Relationship model

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,9 @@
-class MicropostsController < ActiveRecord::database
+class MicropostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :correst_user, only: :destroy
+  before_action :correct_user, only: :destroy
 
   def create
+
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
@@ -28,3 +29,4 @@ class MicropostsController < ActiveRecord::database
     @micropost = current_user.microposts.find_by(id: params[:id])
     redirect_to root_url if @micropost.nil?
   end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationships", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -9,6 +14,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(othetr_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
 end
